2dd9437aedf25d5b6e58407b2ecdab949a1defc3
Michael Buesch <m@bues.ch>
2022-07-09 20:08:58 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="bme280-upy",
+    version="1.0.0",
+    description="BME 280 device driver with Micropython support",
+    author="Michael Büsch",
+    author_email="m@bues.ch",
+    url="https://bues.ch/",
+    packages=["bme280"],
+)
+
+# vim: ts=4 sw=4 expandtab
