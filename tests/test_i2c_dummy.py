e33e94dcf6f60a21a055101ae542d147eaa21c52
Michael Buesch <m@bues.ch>
2022-08-28 20:54:31 +0200

    Add tests for micropython mode

--- tests/test_i2c_dummy.py
+++ tests/test_i2c_dummy.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 import bme280
+import machine
 
+# smbus.SMBus
 class SMBusMock:
     def __init__(self, bus):
         assert bus == 42
@@ -25,7 +27,47 @@ class SMBusMock:
                      0x18, 0x18, ]      # hum
         return [ 0, ] * length
 
+# machine.I2C
+class I2CMock(SMBusMock):
+    def __init__(self, index=None, scl=None, sda=None, freq=None):
+        if index is not None:
+            assert index == 42
+        assert freq is not None
+        if isinstance(scl, PinMock):
+            assert scl._pin == 11
+        if isinstance(scl, int):
+            assert scl == 11
+        if isinstance(sda, PinMock):
+            assert sda._pin == 12
+        if isinstance(sda, int):
+            assert sda == 12
+
+    def deinit(self):
+        pass
+
+    def writeto_mem(self, addr, reg, data):
+        self.write_i2c_block_data(addr, reg, data)
+
+    def readfrom_mem(self, addr, reg, length):
+        return bytes(self.read_i2c_block_data(addr, reg, length))
+
+# machine.SoftI2C
+class SoftI2CMock(I2CMock):
+    def __init__(self, **kwargs):
+        assert "index" not in kwargs
+        I2CMock.__init__(self, **kwargs)
+
+# machine.Pin
+class PinMock:
+    OPEN_DRAIN = object()
+
+    def __init__(self, pin, mode, value):
+        assert mode == self.OPEN_DRAIN
+        assert value == 1
+        self._pin = pin
+
 class Test_I2CDummy(TestCase):
+    @patch("bme280.bme280.isMicropython", False)
     @patch("smbus.SMBus", SMBusMock)
     def test_linux(self):
         with bme280.BME280(i2cBus=42) as bme:
@@ -34,5 +76,24 @@ class Test_I2CDummy(TestCase):
                                      humidityOversampling=bme280.OVSMPL_16,
                                      pressureOversampling=bme280.OVSMPL_4)
             #TODO
+
+    @patch("bme280.bme280.isMicropython", True)
+    @patch("machine.I2C", I2CMock, create=True)
+    @patch("machine.SoftI2C", SoftI2CMock, create=True)
+    @patch("machine.Pin", PinMock, create=True)
+    def test_micropython(self):
+        with bme280.BME280(i2cBus=42) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(i2cBus={ "index": 42, "scl": 11, "sda": 12 }) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(i2cBus={ "scl": 11, "sda": 12 }) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(i2cBus=machine.I2C(42, scl=11, sda=12, freq=100000)) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(i2cBus=machine.I2C(42,
+                                              scl=machine.Pin(11, mode=machine.Pin.OPEN_DRAIN, value=1),
+                                              sda=machine.Pin(12, mode=machine.Pin.OPEN_DRAIN, value=1),
+                                              freq=100000)) as bme:
+            t, h, p = bme.readForced()
 
 # vim: ts=4 sw=4 expandtab
--- tests/test_spi_dummy.py
+++ tests/test_spi_dummy.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 import bme280
+import machine
 
+# spidev.SpiDev
 class SpiDevMock:
     def open(self, bus, cs):
         assert bus == 42 and cs == 2
@@ -25,7 +27,73 @@ class SpiDevMock:
                                    0x18, 0x18, ])     # hum
         return bytes([ 0, ] * length)
 
+# machine.SPI
+class SPIMock(SpiDevMock):
+    MSB = object()
+
+    def __init__(self, index=None, baudrate=None, *,
+                 polarity=None, phase=None, bits=None, firstbit=None,
+                 sck=None, mosi=None, miso=None):
+        if index is not None:
+            assert index == 42
+        if polarity is not None:
+            assert polarity == 0
+        if phase is not None:
+            assert phase == 0
+        if bits is not None:
+            assert bits == 8
+        if firstbit is not None:
+            assert firstbit == self.MSB
+        if isinstance(sck, PinMock):
+            assert sck._pin == 21
+            assert sck._mode == PinMock.OUT
+            assert sck._value == 0
+        if isinstance(sck, int):
+            assert sck == 21
+        if isinstance(mosi, PinMock):
+            assert mosi._pin == 22
+            assert mosi._mode == PinMock.OUT
+            assert mosi._value == 0
+        if isinstance(mosi, int):
+            assert mosi == 22
+        if isinstance(miso, PinMock):
+            assert miso._pin == 23
+            assert miso._mode == PinMock.IN
+            assert miso._value is None
+        if isinstance(miso, int):
+            assert miso == 23
+
+    def deinit(self):
+        pass
+
+    def write(self, data):
+        self.xfer2(data)
+
+    def read(self, nbytes, write=0):
+        return self.xfer2(bytes([write, ] * nbytes))
+
+# machine.SoftSPI
+class SoftSPIMock(SPIMock):
+    def __init__(self, **kwargs):
+        assert "index" not in kwargs
+        SPIMock.__init__(self, **kwargs)
+
+# machine.Pin
+class PinMock:
+    OUT = object()
+    IN = object()
+
+    def __init__(self, pin, mode, value=None):
+        self._value = value
+        self._mode = mode
+        self._pin = pin
+
+    def __call__(self, value=None):
+        assert value is not None
+        self._value = value
+
 class Test_SPIDummy(TestCase):
+    @patch("bme280.bme280.isMicropython", False)
     @patch("spidev.SpiDev", SpiDevMock)
     def test_linux(self):
         with bme280.BME280(spiBus=42, spiCS=2) as bme:
@@ -34,5 +102,25 @@ class Test_SPIDummy(TestCase):
                                      humidityOversampling=bme280.OVSMPL_16,
                                      pressureOversampling=bme280.OVSMPL_4)
             #TODO
+
+    @patch("bme280.bme280.isMicropython", True)
+    @patch("machine.SPI", SPIMock, create=True)
+    @patch("machine.SoftSPI", SoftSPIMock, create=True)
+    @patch("machine.Pin", PinMock, create=True)
+    def test_micropython(self):
+        with bme280.BME280(spiBus=42, spiCS=2) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(spiBus={ "index": 42, "sck": 21, "mosi": 22, "miso": 23 }, spiCS=2) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(spiBus={ "sck": 21, "mosi": 22, "miso": 23 }, spiCS=2) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(spiBus=machine.SPI(42, sck=21, mosi=22, miso=23), spiCS=2) as bme:
+            t, h, p = bme.readForced()
+        with bme280.BME280(spiBus=machine.SPI(42,
+                                              sck=machine.Pin(21, mode=machine.Pin.OUT, value=0),
+                                              mosi=machine.Pin(22, mode=machine.Pin.OUT, value=0),
+                                              miso=machine.Pin(23, mode=machine.Pin.IN)),
+                           spiCS=machine.Pin(2, mode=machine.Pin.OUT, value=1)) as bme:
+            t, h, p = bme.readForced()
 
 # vim: ts=4 sw=4 expandtab
